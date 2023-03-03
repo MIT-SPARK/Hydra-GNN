94e419f9f8af4cafe98443e86f691df89908e292
siyi <siyi@mit.edu>
2023-03-03 18:15:44 -0500

    add Stanford data path to utils

--- hydra_gnn/utils.py
+++ hydra_gnn/utils.py
@@ -15,6 +15,7 @@ MP3D_HOUSE_DIR = os.path.join(DATA_DIR, "house_files")
 MP3D_OBJECT_LABEL_DATA_PATH = os.path.join(DATA_DIR, "mpcat40.tsv")
 HYDRA_TRAJ_DIR = os.path.join(DATA_DIR, "tro_graphs_2022_09_24")
 COLORMAP_DATA_PATH = os.path.join(DATA_DIR, "colormap.csv")
+STANFORD3DSG_DATA_DIR = os.path.join(PROJECT_DIR, "data/Stanford3DSceneGraph/tiny/verified_graph")
 WORD2VEC_MODEL_PATH = os.path.join(DATA_DIR, "GoogleNews-vectors-negative300.bin")
 
 
