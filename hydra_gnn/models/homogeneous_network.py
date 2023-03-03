bb7e8061716acd3487d57298f28cff77b6aecee8
siyi <siyi@mit.edu>
2023-03-03 18:18:44 -0500

    add semisupervised training job and modify homogeneous network

--- hydra_gnn/models/homogeneous_network.py
+++ hydra_gnn/models/homogeneous_network.py
@@ -28,26 +28,33 @@ class HomogeneousNetwork(nn.Module):
         if output_dim is not None:
             assert output_dim_dict is None
             self.classification_task = 'room'
+            if self.conv_block[:3] != 'GAT':
+                mp_output_dim = output_dim
+            else:
+                GAT_hidden_dims_with_last_layer = GAT_hidden_dims + [output_dim]
         else:
             assert output_dim_dict is not None
             self.classification_task = 'all'
-            output_dim = hidden_dim
+            if self.conv_block[:3] != 'GAT':
+                mp_output_dim = hidden_dim
+            else:
+                GAT_hidden_dims_with_last_layer = GAT_hidden_dims
         self.num_layers = num_layers if conv_block[:3] != 'GAT' else len(GAT_heads)
         self.dropout = dropout
 
         # message passing
         self.convs = nn.ModuleList()
         if self.conv_block == 'GAT':
-            self.convs = build_GAT_conv_layers(input_dim, GAT_hidden_dims + [output_dim], GAT_heads,
+            self.convs = build_GAT_conv_layers(input_dim, GAT_hidden_dims_with_last_layer, GAT_heads,
                 GAT_concats, dropout=dropout)
         elif self.conv_block == 'GAT_edge':
-            self.convs = build_GAT_conv_layers(input_dim, GAT_hidden_dims + [output_dim], GAT_heads,
+            self.convs = build_GAT_conv_layers(input_dim, GAT_hidden_dims_with_last_layer, GAT_heads,
                 GAT_concats, dropout=dropout, edge_dim=3, add_self_loop=True, fill_value=torch.zeros(3, dtype=torch.float64))
         else:  # GAT dimensions are different than others
             self.convs.append(build_conv_layer(self.conv_block, input_dim, hidden_dim))
             for _ in range(1, self.num_layers - 1):
                 self.convs.append(build_conv_layer(self.conv_block, hidden_dim, hidden_dim))
-            self.convs.append(build_conv_layer(self.conv_block, hidden_dim, output_dim))
+            self.convs.append(build_conv_layer(self.conv_block, hidden_dim, mp_output_dim))
 
         # batch normalization
         if self.conv_block == 'GIN':
@@ -59,9 +66,13 @@ class HomogeneousNetwork(nn.Module):
             if self.conv_block[:3] != 'GAT':
                 final_hidden_dim = hidden_dim
             else:
-                final_hidden_dim = GAT_hidden_dims[-1] * GAT_heads[-1] if GAT_concats[-1] else GAT_hidden_dims[-1]
-            self.post_mp_room = nn.Linear(final_hidden_dim, output_dim_dict['rooms'])
-            self.post_mp_object = nn.Linear(final_hidden_dim, output_dim_dict['objects'])
+                final_hidden_dim = GAT_hidden_dims_with_last_layer[-1] * GAT_heads[-1] if GAT_concats[-1] \
+                    else GAT_hidden_dims_with_last_layer[-1]
+            # remove 's' for HomogeneousNeuralTreeNetwork child class
+            num_room_labels = output_dim_dict['rooms'] if 'rooms' in output_dim_dict else output_dim_dict['room']
+            num_object_labels = output_dim_dict['objects'] if 'objects' in output_dim_dict else output_dim_dict['object']
+            self.post_mp_room = nn.Linear(final_hidden_dim, num_room_labels)
+            self.post_mp_object = nn.Linear(final_hidden_dim, num_object_labels)
 
     def forward(self, data):
         x, edge_index, room_mask = data.x, data.edge_index, data.room_mask
