6f6d856963e7628486b17252a496021b23d29bad
siyi <siyi@mit.edu>
2023-03-27 21:50:13 +0000

    add helper functions in deprecated to hydra_gnn/utils.py

--- hydra_gnn/utils.py
+++ hydra_gnn/utils.py
@@ -1,9 +1,9 @@
 from neural_tree.construct import HTREE_NODE_TYPES, HTREE_EDGE_TYPES
-from datasets.mp3d import extract_object_graph, extract_room_graph
 import numpy as np
 import torch_geometric
 from torch_geometric.utils import to_networkx
-from torch_geometric.data import HeteroData
+from torch_geometric.data import HeteroData, Data
+import torch
 import networkx as nx
 from networkx.algorithms.approximation.treewidth import treewidth_min_degree, treewidth_min_fill_in
 import plotly.graph_objects as go
@@ -38,6 +38,50 @@ def update_existing_keys(dict_to_update, dict_input):
 # -----------------------------------------------------------------------------
 # Data analysis: graph treewidth, htree diameter
 # -----------------------------------------------------------------------------
+def extract_object_graph(graph_torch, to_nx=True):
+    if not graph_torch['objects']:
+        object_graph = Data()
+        if to_nx:
+            object_graph = nx.empty_graph()
+    else:
+        if graph_torch['objects', 'objects_to_objects', 'objects']:
+            object_graph = Data(x=graph_torch['objects'].x,
+                                pos=graph_torch['objects'].pos,
+                                y=graph_torch['objects'].label,
+                                edge_index=graph_torch['objects', 'objects_to_objects', 'objects']['edge_index'])
+        else:
+            object_graph = Data(x=graph_torch['objects'].x,
+                                pos=graph_torch['objects'].pos,
+                                y=graph_torch['objects'].label,
+                                edge_index=torch.zeros(2, 0).to(dtype=torch.long))
+        if to_nx:
+            object_graph = to_networkx(object_graph).to_undirected()
+
+    return object_graph
+
+
+def extract_room_graph(graph_torch, to_nx=True):
+    if not graph_torch['rooms']:
+        room_graph = Data()
+        if to_nx:
+            room_graph = nx.empty_graph()
+    else:
+        if graph_torch['rooms', 'rooms_to_rooms', 'rooms']:
+            room_graph = Data(x=graph_torch['rooms'].x,
+                              pos=graph_torch['rooms'].pos,
+                              y=graph_torch['rooms'].label,
+                              edge_index=graph_torch['rooms', 'rooms_to_rooms', 'rooms']['edge_index'])
+        else:
+            room_graph = Data(x=graph_torch['rooms'].x,
+                              pos=graph_torch['rooms'].pos,
+                              y=graph_torch['rooms'].label,
+                              edge_index=torch.zeros(2, 0).to(dtype=torch.long))
+        if to_nx:
+            room_graph = to_networkx(room_graph).to_undirected()
+
+    return room_graph
+
+
 def get_treewidth(torch_data, graph_type='full'):
     """
     Find treewidth upper bound of the full input PyG graph, or the object subgraphs, or the room subgraph,
@@ -63,6 +107,7 @@ def get_treewidth(torch_data, graph_type='full'):
             return 0
     else:
         return 0
+
 
 def get_diameters(torch_data, valid_room_labels=None):
     """
