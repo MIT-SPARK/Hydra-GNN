fbf30b799046d09ddf928d42c3bde090bea67e43
Nathan Hughes <na26933@mit.edu>
2023-10-02 16:53:33 +0000

    (wip) start reorg

--- scripts/visualize_dsg.py
+++
@@ -1,9 +0,0 @@
-import spark_dsg as dsg
-import click
-
-
-@click.command()
-@click.argument("dsg_path", type=click.Path(exists=True))
-def main(dsg_path):
-    G = dsg.DynamicSceneGraph.load(dsg_path)
-    dsg.render_to_open3d(G)
